ddd71111cf82677278c0e418d0f34b58b46bbe7c
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-23 14:44:58 -0500

    Add files via upload

---
+++ 2condicionales.js
@@ -0,0 +1,29 @@
+let imc = Number (prompt("ingrese su peso en Kg"))
+if(imc< 18.5)
+{
+    console.log("usted esta bajo de peso")
+}else{
+    if(imc <25)
+    {console.log("su peso es normal")
+}else{
+     if(imc < 30)
+    {console.log("preobesidad")
+ }else{
+     if(imc ==30)
+    {console.log("obesidad")
+ }else{
+     if(imc<35 )
+     {console.log("obesidad de clase 1")
+ }else{
+     if(imc < 40)
+     {console.log("obesidad de clase 2")
+ }else{
+     console.log("obesidad de clase 3")
+ }
+ }
+ }
+    
+}    
+}
+}
+    
