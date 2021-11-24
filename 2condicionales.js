0868690949c91405282ef611bbeb1e6c10f7c688
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-23 20:41:37 -0500

    Update 2condicionales.js

--- 2condicionales.js
+++ 2condicionales.js
@@ -1,25 +1,24 @@
 let h1informacion=document.querySelector("#parrafopeso")
 let imc = Number (prompt("ingrese su peso en Kg"))
 if(imc< 18.5)
-{
-    h1informacion.innerHTML=("usted esta bajo de peso")
+    {alert("usted esta bajo de peso")
 }else{
     if(imc <25)
-    {console.log("su peso es normal")
+    {alert("su peso es normal")
 }else{
      if(imc < 30)
-    {console.log("preobesidad")
+    {alert("preobesidad")
  }else{
      if(imc ==30)
-    {console.log("obesidad")
+    {alert("obesidad")
  }else{
      if(imc<35 )
-     {console.log("obesidad de clase 1")
+     {alert("obesidad de clase 1")
  }else{
      if(imc < 40)
-     {console.log("obesidad de clase 2")
+     {alert("obesidad de clase 2")
  }else{
-     console.log("obesidad de clase 3")
+     alert("obesidad de clase 3")
  }
  }
  }
