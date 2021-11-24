4f87df5f9be42ced8b5ac329a52a03c794cde37a
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-23 20:38:32 -0500

    Update 2condicionales.js

--- 2condicionales.js
+++ 2condicionales.js
@@ -2,7 +2,7 @@ let h1informacion=document.querySelector("#parrafopeso")
 let imc = Number (prompt("ingrese su peso en Kg"))
 if(imc< 18.5)
 {
-    console.log("usted esta bajo de peso")
+    h1informacion.innerHTML=("usted esta bajo de peso")
 }else{
     if(imc <25)
     {console.log("su peso es normal")
