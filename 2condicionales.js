1e91052d587150fc9ed870dae927bfb100fb0f53
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-30 14:58:30 -0500

    Update 2condicionales.js

--- 2condicionales.js
+++ 2condicionales.js
@@ -1,23 +1,23 @@
 let imc = Number (prompt("ingrese su peso en Kg"))
 if(imc< 18.5)
-    {respuesta="usted esta bajo de peso"
+    { respuesta="usted esta bajo de peso"
 }else{
     if(imc <25)
-    {respuesta="su peso es normal"
+    { respuesta="su peso es normal"
 }else{
      if(imc < 30)
-    {respuesta="preobesidad"
+    { respuesta="preobesidad"
  }else{
      if(imc ==30)
-    {respuesta="obesidad"
+    { respuesta="obesidad"
  }else{
      if(imc<35 )
-     {respuesta="obesidad de clase 1"
+     { respuesta="obesidad de clase 1"
  }else{
      if(imc < 40)
-     {respuesta="obesidad de clase 2"
+     { respuesta="obesidad de clase 2"
  }else{
-     respuesta="obesidad de clase 3"
+      respuesta="obesidad de clase 3"
  }
  }
  }
